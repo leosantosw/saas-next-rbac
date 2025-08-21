b6b51f5165a1e277dea6add15ac38a9775060a70
leosantosw <leonardosantosx0077@gmail.com>
2025-08-21 00:08:42 -0300

    docs: implement swagger with scalar

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,20 +1,36 @@
 import { fastify } from 'fastify'
 import fastifyCors from '@fastify/cors'
 import {
-  //   jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import fastifySwagger from '@fastify/swagger'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.register(fastifyCors)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'RBAC Node API',
+      description: 'Full stack app',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
 
+app.register(import('@scalar/fastify-api-reference'), {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyCors)
 app.register(createAccount)
 
 app.listen({ port: 4000 }).then(() => {
