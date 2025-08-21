9d9c724e71b6987af669243c662f5296971f22a2
leosantosw <leonardosantosx0077@gmail.com>
2025-08-20 23:00:53 -0300

    feat: create account

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +16,27 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'ok'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+      const userWithSameEmail = await prisma.user.findFirst({
+        where: { email },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(409).send({ message: 'Email already in use' })
+      }
+
+      const passwordHash = await hash(password, 8)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send({ message: 'User created successfully' })
     }
   )
 }
