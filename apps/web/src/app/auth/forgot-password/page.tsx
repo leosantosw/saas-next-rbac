3aed60c837ec1e4c85b7255a595d0c8a37dc52b5
leosantosw <leonardosantosx0077@gmail.com>
2025-08-24 11:16:15 -0300

    feat: implement login/register page

---
+++ apps/web/src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import Link from 'next/link'
+
+export default function ForgotPasswordPage() {
+  return (
+    <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input id="email" name="email" type="email" />
+      </div>
+
+      <Button className="w-full">Recover password</Button>
+
+      <Button className="w-full" size="sm" variant="link">
+        <Link href="/auth/sign-in">Sign in instead</Link>
+      </Button>
+    </form>
+  )
+}
---
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -0,0 +1,49 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import Link from 'next/link'
+import githubIcon from '@/assets/github-icon.svg'
+import Image from 'next/image'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignInPage() {
+  return (
+    <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input id="name" name="name" type="name" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input id="email" name="email" type="email" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" name="password" type="password" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="confirm-password">Confirm your password</Label>
+        <Input id="confirm-password" name="confirm-password" type="password" />
+      </div>
+
+      <Button className="w-full">Create account</Button>
+
+      <Button className="w-full" size="sm" variant="link">
+        <Link href="/auth/sign-in">Already have an account? Sign in</Link>
+      </Button>
+
+      <Separator />
+      <Button className="w-full" variant="outline">
+        <Image
+          src={githubIcon}
+          alt="Github"
+          className="mr-2 size-4 dark:invert"
+        />
+        Sign up with Github
+      </Button>
+    </form>
+  )
+}
