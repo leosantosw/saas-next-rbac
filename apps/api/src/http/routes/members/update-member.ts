c6407e10214fab1b9793ec16ca952c4b2d34ff14
leosantosw <leonardosantosx0077@gmail.com>
2025-08-23 22:16:16 -0300

    feat: implement member module

---
+++ apps/api/src/http/routes/members/update-member.ts
@@ -0,0 +1,64 @@
+import { rolesSchema } from '@repo/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { UnauthorizedError } from '@/http/routes/errors/unauthorized-error'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+export async function updateMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['Members'],
+          summary: 'Update a member',
+          description:
+            'Updates the role of a specific member within an organization.',
+          params: z.object({
+            slug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          body: z.object({
+            role: rolesSchema,
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+        const userId = await request.currentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new UnauthorizedError(
+            `You're not allowed to update this member.`
+          )
+        }
+
+        const { role } = request.body
+
+        await prisma.member.update({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+          data: {
+            role,
+          },
+        })
+
+        return reply.status(204).send()
+      }
+    )
+}
