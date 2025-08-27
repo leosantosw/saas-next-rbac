e18af21c24427be8c43889451c59be158899baad
leosantosw <leonardosantosx0077@gmail.com>
2025-08-27 00:07:21 -0300

    feat: implement toggle theme

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { ThemeProvider } from 'next-themes'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -11,8 +12,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className="dark">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
+      </body>
     </html>
   )
 }
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -4,6 +4,8 @@ import { ProfileButton } from './profile-button'
 import { Slash } from 'lucide-react'
 import { OrganizationSwitcher } from './organization-switcher'
 import { ability } from '@/auth/auth'
+import { ThemeSwitcher } from './theme/theme-switcher'
+import { Separator } from './ui/separator'
 
 export async function Header() {
   const permissions = await ability()
@@ -11,12 +13,17 @@ export async function Header() {
     <div className="mx-auto flex max-w-[1200px] justify-between">
       <div className="flex items-center gap-3">
         <Image src={logo} alt="logo" className="size-8 dark:invert" />
+
         <Slash className="text-border size-5 -rotate-[24deg]" />
 
         <OrganizationSwitcher />
+
         {permissions?.can('get', 'Project') && <p>Project</p>}
       </div>
-      <div className="">
+
+      <div className="flex items-center gap-4 ">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" style={{ height: '1.8rem' }} />
         <ProfileButton />
       </div>
     </div>
