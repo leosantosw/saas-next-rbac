e6375a6cf4cf4a686f56deadbd1d3bca08e542da
leosantosw <leonardosantosx0077@gmail.com>
2025-08-27 00:23:57 -0300

    fix: resolve theme switcher

--- apps/web/src/components/theme/theme-switcher.tsx
+++ apps/web/src/components/theme/theme-switcher.tsx
@@ -9,15 +9,28 @@ import {
   DropdownMenuContent,
 } from '../ui/dropdown-menu'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export function ThemeSwitcher() {
   const { resolvedTheme, setTheme } = useTheme()
+
+  const [isMounted, setIsMounted] = useState(false)
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          {resolvedTheme === 'light' && <Sun className="size-4" />}
-          {resolvedTheme === 'dark' && <Moon className="size-4" />}
+          {resolvedTheme === 'light' ? (
+            <Sun className="size-4" />
+          ) : (
+            <Moon className="size-4" />
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
